e323679c1a4cf5709986c4224687af4275ecc941
lap777777 <verzus.mining@gmail.com>
2021-09-30 09:40:16 +0200

    dokonceni 2 casti tutorialu prace s modely

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.apps.PollsConfig",
+    "polls.apps.PollsConfig", # pridani aplikace polls do projektu ... inicializace
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
