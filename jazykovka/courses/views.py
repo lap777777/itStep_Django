be1129fe41f58ab86ae82a89d71b13f057b83899
lap777777 <verzus.mining@gmail.com>
2021-10-03 17:56:30 +0200

    dokoncena aplikace jazykovka

--- jazykovka/courses/views.py
+++ jazykovka/courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 from . import models
 
 # Create your views here.
@@ -34,3 +35,58 @@ class BranchDetailView(DetailView):
     template_name = "branch_detail.html"
     model = models.Branch
 
+class ApplicationCreateView(CreateView):
+    template_name = "application_create.html"
+    model = models.Application
+    fields = ["first_name", "last_name", "email", "course"]
+    success_url = reverse_lazy("application_confirmation")
+  
+class ApplicationCreateView1(CreateView):
+    template_name = "application_create1.html"
+    model = models.Application
+    fields = ["first_name", "last_name", "email", "course"]
+    success_url = reverse_lazy("application_confirmation")
+    def from_valid(self, form):
+        course_id = self.kwargs["pk"]
+        course = models.Course.object.get(pk=course_id)
+        form.instance.course = course
+        return super().form_valid(form)
+
+class ApplicationConfirmation(TemplateView):
+    template_name = "application_confirmation.html"
+
+class RegisterCreateView(CreateView):
+    template_name = "registration.html"
+    model = models.Registration
+    fields = ["name", "surname", "email", "birth"]
+    success_url = reverse_lazy("registration_confirmation")
+
+class RegistrationConfirmation(TemplateView):
+    template_name = "registration_confirmation.html"
+
+class ApplicationsListView(ListView):
+    template_name = "application_list.html"
+    model = models.Application
+    def get_queryset(self):
+        cislo_kurzu = self.kwargs['pk']
+        query_set = models.Application.objects.filter(course_id=cislo_kurzu)
+        return query_set
+    # get_query nastavuje odkud bude brat object data
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cislo_kurzu"] = models.Course.objects.get(pk=self.kwargs['pk'])
+        return context
+
+class BranchApplicationListView(ListView):
+    template_name = "branch_application_list.html"
+    model = models.Course
+    def get_queryset(self):
+        cislo_pobocky = self.kwargs['pk']
+        query_set = models.Course.objects.filter(branch_id=cislo_pobocky)
+        return query_set
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cislo_pobocky"] = models.Branch.objects.get(pk=self.kwargs['pk'])
+        return context
--- jazykovka/jazykovka/settings.py
+++ jazykovka/jazykovka/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "courses.apps.CoursesConfig",
+    "bootstrap4",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
