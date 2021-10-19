0638e697b5d5bc939d1bb3f5c2ec7eacd47e46e1
lap777777 <verzus.mining@gmail.com>
2021-10-19 21:32:31 +0200

    projekt skauti - relations

--- skauti/evidence/admin.py
+++ skauti/evidence/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
-from .models import Clen
+from .models import Bobrik, Clen, Oddil, Adresa, Bobrik
 
 # Register your models here.
 
 class ClenAdmin(admin.ModelAdmin):
     list_display = ("jmeno", "prijmeni", "prezdivka", "vek", "prispevek")
     list_filter = ("prezdivka",)
+    prepopulated_fields = { "slug": ("prezdivka", )}
+
+class OddilAdmin(admin.ModelAdmin):
+    list_display = ("jmeno", "vlajka", "sidlo")
+
+class AdresaAdmin(admin.ModelAdmin):
+    list_display = ("ulice", "cislo", "mesto")
+
+class BobrikAdmin(admin.ModelAdmin):
+    list_display = ("dovednost",)
 
 admin.site.register(Clen, ClenAdmin)
+admin.site.register(Oddil, OddilAdmin)
+admin.site.register(Adresa, AdresaAdmin)
+admin.site.register(Bobrik, BobrikAdmin)
