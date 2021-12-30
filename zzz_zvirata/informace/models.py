9f7cff064268aa1e231a643eda0f88b8a8b493a9
lap777777 <verzus.mining@gmail.com>
2021-12-30 12:30:27 +0100

    static + modely

--- zzz_zvirata/informace/models.py
+++ zzz_zvirata/informace/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Zvire(models.Model):
+    jmeno = models.CharField(max_length=30)
+    vaha = models.IntegerField()
+    barva = models.CharField(max_length=40)
+    zije = models.BooleanField(null=True)
+
+    def __str__(self):
+        return f"Jmeno: {self.jmeno}, vaha: {self.vaha}, barva: {self.barva}"
